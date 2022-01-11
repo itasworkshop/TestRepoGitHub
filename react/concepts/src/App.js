0d3429ca9af62798661ba35864fa1358d9a46a5a
itasworkshop <itasworkshop@gmail.com>
2022-01-11 17:00:34 +0530

    11 Jan

---
+++ react/concepts/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+//JSX --> mixture of JS and HTML
+//App --> Function based component
+function App() {
+  return (
+    <div>
+      <h1>Hello World!!!!</h1>
+      <h1>Hello World!!!!</h1>
+    </div>    
+  );
+}
+
+export default App;
